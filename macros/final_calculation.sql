232c5853dde7269c519f0b8a2a874d3b46a6684b
Peter Wang <pw33537515@gmail.com>
2026-04-29 16:29:57 +1200

    fix element extraction

--- macros/final_calculation.sql
+++ macros/final_calculation.sql
@@ -89,21 +89,12 @@ SELECT * EXCEPT(ad_name), ad_name AS creative_name,
         THEN 'Social Display'
         ELSE 'Other'
     END AS media_format,
-    CASE WHEN ARRAY_LENGTH(SPLIT(media_buy_name, '_')) < 8 AND ARRAY_LENGTH(SPLIT(media_buy_name, '_')) > 1  
-         THEN SPLIT(media_buy_name, '_')[SAFE_OFFSET(ARRAY_LENGTH(SPLIT(media_buy_name, '_'))-1)] 
-         WHEN ARRAY_LENGTH(SPLIT(media_buy_name, '_')) >= 8 THEN SPLIT(media_buy_name, '_')[SAFE_OFFSET(7)] 
-         ELSE 'Other' END AS audience_name,
-    CASE WHEN ARRAY_LENGTH(SPLIT(ad_name, '_')) < 8 AND ARRAY_LENGTH(SPLIT(ad_name, '_')) > 1  
-         THEN SPLIT(ad_name, '_')[SAFE_OFFSET(ARRAY_LENGTH(SPLIT(ad_name, '_'))-1)] 
-         WHEN ARRAY_LENGTH(SPLIT(ad_name, '_')) >= 8 THEN SPLIT(ad_name, '_')[SAFE_OFFSET(7)] 
+    REGEXP_EXTRACT(media_buy_name, r'PLATFORM_([^_]+)') AS audience_name,
+    CASE WHEN ARRAY_LENGTH(SPLIT(ad_name, '_')) >= 8 THEN SPLIT(ad_name, '_')[SAFE_OFFSET(7)] 
          ELSE 'Other' END AS creative_descr,
     CASE WHEN ARRAY_LENGTH(SPLIT(ad_name, '_')) >= 8 THEN SPLIT(ad_name, '_')[SAFE_OFFSET(5)] 
-         WHEN ARRAY_LENGTH(SPLIT(ad_name, '_')) < 8 AND ARRAY_LENGTH(SPLIT(ad_name, '_')) > 1  
-         THEN SPLIT(ad_name, '_')[SAFE_OFFSET(ARRAY_LENGTH(SPLIT(ad_name, '_'))-3)] 
          ELSE 'Other' END AS ad_format_detail,
     CASE WHEN ARRAY_LENGTH(SPLIT(ad_name, '_')) >= 8 THEN SPLIT(ad_name, '_')[SAFE_OFFSET(6)] 
-         WHEN ARRAY_LENGTH(SPLIT(ad_name, '_')) < 8 AND ARRAY_LENGTH(SPLIT(ad_name, '_')) > 1  
-         THEN SPLIT(ad_name, '_')[SAFE_OFFSET(ARRAY_LENGTH(SPLIT(ad_name, '_'))-2)] 
          ELSE 'Other' END AS ad_format,
     CASE WHEN ARRAY_LENGTH(SPLIT(campaign_name,'_')) <=1 THEN 'Other'
         ELSE SPLIT(campaign_name,'_')[SAFE_OFFSET(1)] END AS campaign_descr
